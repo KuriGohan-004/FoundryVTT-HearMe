5764bcef72aa9d78255318e78cc1a028dbde1b68
KuriGohan-004 <KuriGohan_004@mail.com>
2025-12-18 23:24:55 +0000

    Update z-order.js

--- scripts/z-order.js
+++ scripts/z-order.js
@@ -1,12 +1,22 @@
 function updateTokenSort(token) {
   if (!token) return;
-  token.sort = Math.round(token.y);
+  const y = Math.round(token.y);
+  if (token.document.sort === y) return;
+  token.document.update({ sort: y });
 }
 
 Hooks.on("canvasReady", () => {
   canvas.tokens.placeables.forEach(updateTokenSort);
 });
 
-Hooks.on("refreshToken", (token) => {
-  updateTokenSort(token);
+Hooks.on("preUpdateToken", (doc, change) => {
+  if (change.x !== undefined || change.y !== undefined) {
+    const token = doc.object;
+    if (!token) return;
+
+    const y = Math.round(change.y ?? token.y);
+    if (doc.sort === y) return;
+
+    doc.update({ sort: y });
+  }
 });
